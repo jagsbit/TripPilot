15c16c783f872ca0fb48ce665de902e720896778
Jagannath Nayak <jagannath_nayak@epam.com>
2026-09-21 11:58:19 +0530

    project setup

---
+++ src/main/java/com/project/trippilot/TrippilotApplication.java
@@ -0,0 +1,13 @@
+package com.project.trippilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrippilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrippilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/trippilot/TrippilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.trippilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrippilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
